2a6a1e5318a627f36ed58d251bfcbde333b7f9e7
ArtemSydun <sydun2003@gmail.com>
2023-03-02 22:57:10 +0200

    adaptive

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+window.addEventListener('hashchange', () => {
+    if (window.location.hash === '#menu') {
+        document.body.classList.add('menu')
+    }
+    else {
+        document.body.classList.remove('menu')
+    }
+});
